6eb76bc7a2b56e3ab3636c13e933bd7e487b71de
Andrew Stoker <andy.stoker@icloud.com>
2025-06-06 14:14:59 -0400

    WIP react builder

---
+++ src/documents/editor/EditorContext.tsx
@@ -0,0 +1,110 @@
+import { create } from 'zustand';
+
+import { TEditorConfiguration } from './core';
+
+/**
+ * Type definition for the editor state.
+ */
+type EditorState = {
+  document: TEditorConfiguration;
+
+  selectedBlockId: string | null;
+  selectedSidebarTab: 'block-configuration' | 'styles';
+  selectedMainTab: 'editor' | 'preview' | 'json' | 'html';
+  selectedScreenSize: 'desktop' | 'mobile';
+
+  inspectorDrawerOpen: boolean;
+};
+
+const editorStateStore = create<EditorState>(() => ({
+  document: {
+    root: {
+      type: 'EmailLayout',
+      data: {
+        backdropColor: '#F5F5F5',
+        canvasColor: '#FFFFFF',
+        textColor: '#262626',
+        fontFamily: 'MODERN_SANS',
+        childrenIds: [],
+      }
+    }
+  },
+  selectedBlockId: null,
+  selectedSidebarTab: 'styles',
+  selectedMainTab: 'editor',
+  selectedScreenSize: 'desktop',
+
+  inspectorDrawerOpen: true,
+} as EditorState));
+
+export function useDocument() {
+  return editorStateStore((s) => s.document);
+}
+
+export function useSelectedBlockId() {
+  return editorStateStore((s) => s.selectedBlockId);
+}
+
+export function useSelectedScreenSize() {
+  return editorStateStore((s) => s.selectedScreenSize);
+}
+
+export function useSelectedMainTab() {
+  return editorStateStore((s) => s.selectedMainTab);
+}
+
+export function setSelectedMainTab(selectedMainTab: EditorState['selectedMainTab']) {
+  return editorStateStore.setState({ selectedMainTab });
+}
+
+export function useSelectedSidebarTab() {
+  return editorStateStore((s) => s.selectedSidebarTab);
+}
+
+export function useInspectorDrawerOpen() {
+  return editorStateStore((s) => s.inspectorDrawerOpen);
+}
+
+export function setSelectedBlockId(selectedBlockId: EditorState['selectedBlockId']) {
+  const selectedSidebarTab = selectedBlockId === null ? 'styles' : 'block-configuration';
+  const options: Partial<EditorState> = {};
+  if (selectedBlockId !== null) {
+    options.inspectorDrawerOpen = true;
+  }
+  return editorStateStore.setState({
+    selectedBlockId,
+    selectedSidebarTab,
+    ...options,
+  });
+}
+
+export function setSidebarTab(selectedSidebarTab: EditorState['selectedSidebarTab']) {
+  return editorStateStore.setState({ selectedSidebarTab });
+}
+
+export function resetDocument(document: EditorState['document']) {
+  return editorStateStore.setState({
+    document,
+    selectedSidebarTab: 'styles',
+    selectedBlockId: null,
+  });
+}
+
+export function setDocument(document: EditorState['document']) {
+  const originalDocument = editorStateStore.getState().document;
+  return editorStateStore.setState({
+    document: {
+      ...originalDocument,
+      ...document,
+    },
+  });
+}
+
+export function toggleInspectorDrawerOpen() {
+  const inspectorDrawerOpen = !editorStateStore.getState().inspectorDrawerOpen;
+  return editorStateStore.setState({ inspectorDrawerOpen });
+}
+
+export function setSelectedScreenSize(selectedScreenSize: EditorState['selectedScreenSize']) {
+  return editorStateStore.setState({ selectedScreenSize });
+}
