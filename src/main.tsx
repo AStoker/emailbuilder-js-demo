dc9609295d1adfc98053527dc58b872e8954cd95
Andrew Stoker <andy.stoker@icloud.com>
2025-06-09 15:34:04 -0400

    Add web components

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import r2wc from '@r2wc/react-to-web-component'
+
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import App from './App';
 import theme from './theme';
 
+export function EmailBuilderApp() {
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <EmailBuilderApp />
 );
+
+const EmailBuilderWebComponent = r2wc(EmailBuilderApp);
+customElements.define('email-builder', EmailBuilderWebComponent);
