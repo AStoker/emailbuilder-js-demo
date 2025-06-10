d10de5ec927bbc315916faf6211268a465eaa7cd
Andrew Stoker <andy.stoker@icloud.com>
2025-06-10 11:50:17 -0400

    Working on wrapping in shadows

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import createCache, { EmotionCache } from '@emotion/cache';
 
 import r2wc from '@r2wc/react-to-web-component'
 
@@ -7,15 +8,18 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import App from './App';
 import theme from './theme';
+import { CacheProvider } from '@emotion/react';
 
-export function EmailBuilderApp() {
+export function EmailBuilderApp({ cache }: { cache: EmotionCache }) {
   return (
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>
+    <CacheProvider value={cache}>
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </React.StrictMode>
+    </CacheProvider>
   );
 }
 
@@ -23,5 +27,45 @@ export function EmailBuilderApp() {
 //   <EmailBuilderApp />
 // );
 
-const EmailBuilderWebComponent = r2wc(EmailBuilderApp);
+class EmailBuilderWebComponent extends HTMLElement {
+  private cache: EmotionCache | undefined;
+  private reactRoot: any;
+
+  connectedCallback() {
+    // Create shadow DOM
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    
+    // Create Emotion cache with shadow DOM container
+    this.cache = createCache({
+      key: 'css',
+      prepend: true,
+      container: shadowRoot as any
+    });
+
+    console.log('EmailBuilderWebComponent connected');
+
+    // Create a container div for React
+    const container = document.createElement('div');
+    shadowRoot.appendChild(container);
+
+    // Convert React component to work with shadow DOM
+    const ReactComponent = r2wc(() => <EmailBuilderApp cache={this.cache!} />, {
+      shadow: undefined, // We're handling shadow DOM ourselves
+    });
+
+    // Create and append the React web component
+    const reactElement = new ReactComponent();
+    container.appendChild(reactElement);
+  }
+
+  disconnectedCallback() {
+    // Clean up when component is removed
+    if (this.reactRoot) {
+      this.reactRoot.unmount();
+    }
+  }
+}
+// const EmailBuilderWebComponent = r2wc(EmailBuilderApp, {
+//   shadow: "open",
+// });
 customElements.define('email-builder', EmailBuilderWebComponent);
